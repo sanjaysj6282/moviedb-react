8ec50d3874245a963e7a7a85970ff9ddf94ed5a7
Sanjay <sanjaysj6282@gmail.com>
2022-06-12 12:25:40 +0530

    api added

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function MovieList() {
+    const [result, setResult] = useState([]);
+    const moviesList = async () =>{
+        let data=await fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=22a11be4d14b63a8250c0e0de6d489c4&language=en");
+        let parsedData=data.json;
+        setResult(parsedData.results);
+
+        console.log(result);
+    }
+
   return (
-    <div>MovieList</div>
+    <>
+        {result.map(movie =>{
+            
+        })}
+    </>
   )
 }
 

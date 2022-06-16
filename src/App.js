374e3182d77481d17b3ad7d2e3ac6bc1ecef69e0
Sanjay <sanjaysj6282@gmail.com>
2022-06-16 21:32:49 +0530

    commented api key

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import MovieList from './Components/MovieList';
 
 function App() {
   let APIKEY = process.env.REACT_APP_MOVIEDB_API_KEY
-  console.log(APIKEY);
-  let apiKey = "22a11be4d14b63a8250c0e0de6d489c4";
+  // console.log(APIKEY);
+  // let apiKey = "22a11be4d14b63a8250c0e0de6d489c4";
   return (
     <div className="App">
       <Navbar/>

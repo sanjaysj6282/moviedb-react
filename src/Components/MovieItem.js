f6634fa9a8d6db379d0bef510c3fb01d369b0adb
Sanjay <sanjaysj6282@gmail.com>
2022-06-26 01:33:54 +0530

    default props

--- src/Components/MovieItem.js
+++ src/Components/MovieItem.js
@@ -1,14 +1,30 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 function MovieItem(props) {
+  let defaultImg =
+    "https://image.tmdb.org/t/p/w500//teCy1egGQa0y8ULJvlrDHQKnxBL.jpg";
+  // console.log(props.img);
   return (
     <div>
       <div className="card">
-        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-success" style={{left:"95%"}}>
+        <span
+          className="position-absolute top-0 translate-middle badge rounded-pill bg-success"
+          style={{ left: "95%" }}
+        >
           {props.popularity}
         </span>
-        <img src={props.img} className="card-img-top" alt="..." />
+        {props.img.substring(props.img.length - 4, props.img.length) !=
+        "null" ? (
+          <img src={props.img} className="card-img-top" alt="Image not found" />
+        ) : (
+          <img
+            src={defaultImg}
+            className="card-img-top"
+            alt="Image not found"
+          />
+        )}
         {/* {print()} */}
         <div className="card-body">
           <h5 className="card-title">{props.title}</h5>
@@ -18,7 +34,18 @@ function MovieItem(props) {
               Release date: {props.releasedate}
             </small>
           </p>
-          <Link className="btn btn-dark" to={`/movie_details/${props.title}`} state={{ movieid : props.id, imgposter : props.img}}>
+          <Link
+            className="btn btn-dark"
+            to={`/movie_details/${props.title}`}
+            state={{
+              movieid: props.id,
+              imgposter:
+                props.img.substring(props.img.length - 4, props.img.length) !=
+                "null"
+                  ? props.img
+                  : defaultImg,
+            }}
+          >
             Read more
           </Link>
         </div>
